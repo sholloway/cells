23b9852d02091f8fc715b35f8b5e45f228ea4e08
Samuel Holloway <sholloway@gmail.com>
2019-01-30 13:44:01 -0600

    Added JSDoc comments to the CellEvaluator.

--- conway_module/lib/core/CellEvaluator.js
+++ conway_module/lib/core/CellEvaluator.js
@@ -1,16 +1,25 @@
 const { CellStates } = require('./CellStates.js')
 
-/*
-The Default Evaluator leverages the Game B3/S23.
-Born Requirements: 3 Neighbors
-Survival Requirements: {2, 3} Neighbors
-*/
+/**
+ * Enforces the simulation (game) rules.
+ */
 class CellEvaluator{
+	/**
+	 * Creates a new evaluator.
+	 * @param {number[]} birthRules - The required alive neighbors for a cell to be born.
+	 * @param {number[]} survivalRules - The required alive neighbors for a cell to stay alive.
+	 */
 	constructor(birthRules=[3], survivalRules=[2,3]){
 		this.birthRules = birthRules
 		this.survivalRules = survivalRules
 	}
 
+	/**
+	 * Evaluates a cell's next state.
+	 * @param {number} neighborsCount - The number of a live cells the current cell has.
+	 * @param {CellState} currentCellState - the current state of cell.
+	 * @returns {CellState} The state the cell should be set to.
+	 */
 	evaluate(neighborsCount, currentCellState){
 		let nextCellState;
 		switch (currentCellState){
