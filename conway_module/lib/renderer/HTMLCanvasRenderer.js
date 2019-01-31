286be3c03db59ec162d177dd9801579d34fc199d
Samuel Holloway <sholloway@gmail.com>
2019-01-31 15:16:11 -0600

    Added JSDoc comments to HTMLCanvasRenderer.

--- conway_module/lib/renderer/HTMLCanvasRenderer.js
+++ conway_module/lib/renderer/HTMLCanvasRenderer.js
@@ -1,13 +1,28 @@
+/**
+ * @private
+ */
 function clearCanvas(htmlCanvasContext, config){
   htmlCanvasContext.clearRect(0,0, config.canvas.width, config.canvas.height)
 }
 
+/**
+ * Responsible rendering a scene.
+ */
 class HTMLCanvasRenderer{
+	/**
+	 * Initializes a new HTMLRenderer
+	 * @param {HTMLCanvasContext} htmlCanvasContext
+	 * @param {object} config - The simulation's configuration object.
+	 */
 	constructor(htmlCanvasContext, config){
 		this.htmlCanvasContext = htmlCanvasContext
 		this.config = config
 	}
 
+	/**
+	 * Renders one frame of the scene.
+	 * @param {SceneManager} scene - The scene to render.
+	 */
 	render(scene){
 		this.clear()
 		while(!scene.fullyRendered()){
@@ -16,6 +31,9 @@ class HTMLCanvasRenderer{
 		}
 	}
 
+	/**
+	 * Erases the entire canvas.
+	 */
 	clear(){
 		clearCanvas(this.htmlCanvasContext, this.config)
 	}
