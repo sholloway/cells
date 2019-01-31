d6ffbece519e73c32191974da735dab013cdc32e
Samuel Holloway <sholloway@gmail.com>
2019-01-31 14:23:32 -0600

    Added JSDoc comments.

--- conway_module/lib/core/GridSystem.js
+++ conway_module/lib/core/GridSystem.js
@@ -1,13 +1,25 @@
+/**
+ * A system that draws a uniform grid.
+ */
 class GridSystem{
 	constructor(htmlCanvasContext, cellSize){
 		this.htmlCanvasContext = htmlCanvasContext
 		this.cellSize = cellSize
 	}
 
+	/**
+	 * Set the uniform cell size.
+	 * @param {number} size - The width and height of a cell.
+	 */
 	setCellSize(size){
 		this.cellSize = size
 	}
 
+	/**
+	 * Renders a grid on a HTML Canvas.
+	 * @param {number} width - The total width of the grid.
+	 * @param {*} height - The total height of the grid.
+	 */
 	drawGrid(width, height){
 		this.htmlCanvasContext.strokeStyle = '#757575'
 		this.htmlCanvasContext.lineWidth = 0.5
@@ -26,6 +38,13 @@ class GridSystem{
 		}
 	}
 
+	/**
+	 * Clears a region of the HTML canvas.
+	 * @param {number} x - The left most coordinate of the area to clear.
+	 * @param {number} y - The upper most coordinate of the area to clear.
+	 * @param {number} width - The width of the area to clear.
+	 * @param {number} height - The height of the area to clear.
+	 */
 	clear(x,y, width, height){
 		this.htmlCanvasContext.clearRect(x,y,width,height)
 	}
