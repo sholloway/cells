52a3c1dd93e6e9e89d9d742fa5371492c2645e0b
Samuel Holloway <sholloway@gmail.com>
2019-01-31 14:59:40 -0600

    Added JSDocs to SeederFactory.

--- conway_module/lib/core/SeederFactory.js
+++ conway_module/lib/core/SeederFactory.js
@@ -1,29 +1,66 @@
+/**
+ * Conway's Game Initial State Seeder Module
+ * @module seeders
+ */
 const { CellStates } = require('./CellStates.js')
 const {Cell} = require('./Quadtree.js')
 
+/**
+ * Randomly selects 0 or 1.
+ * @private
+ * @returns {number}
+ */
 function randomAliveOrDead(){
   return getRandomIntInclusive(CellStates.DEAD, CellStates.ALIVE)
 }
 
+/**
+ * Finds a random integer in the set defined by two bounds.
+ * @private
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
 
+/**
+ * Abstract class. Defines a seeder.
+ */
 class Seeder{
+	/**
+	 * Initialize a new seeder.
+	 */
 	constructor(){
 		this.cells = []
 	}
 
+	/**
+	 * The algorithm to seed the simulation with.
+	 * @abstract
+	 * @param {number} width
+	 * @param {number} height
+	 */
 	seed(width, height){
 		throw new Error('Seeder implementations must implement the seed(width, height) method.')
 	}
 
+	/**
+	 * Initial cells to use by the seeder.
+	 * @param {Cell[]} cells
+	 */
 	setCells(cells){
 		this.cells = cells
 	}
 }
+
+/**
+ * Seeds a simulation with randomly selecting alive or dead for each cell.
+ * @extends Seeder
+ */
 class RandomSeeder extends Seeder{
 	constructor(){
 		super()
@@ -42,6 +79,10 @@ class RandomSeeder extends Seeder{
 	}
 }
 
+/**
+ * Seeds a simulation with a provided set of alive cells.
+ * @extends Seeder
+ */
 class StaticCellsSeeder extends Seeder{
 	constructor(){
 		super()
@@ -52,15 +93,23 @@ class StaticCellsSeeder extends Seeder{
 	}
 }
 
+/**
+ * The set of supported seeder models.
+ */
 SeederModels = {
 	DRAWING: 'draw',
 	RANDOM: 'random'
 }
 
+/**
+ * Creates a new seeder based on a specified seeder model name.
+ */
 class SeederFactory{
-	constructor(){
-	}
-
+	/**
+	 * Initializes a new seeder.
+	 * @param {string} modelName
+	 * @returns {Seeder}
+	 */
 	static build(modelName){
 		let seeder = null;
 		switch (modelName){
