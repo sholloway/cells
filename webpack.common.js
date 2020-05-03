460d843d1bfb4b05e5461028df48bf88932b7419
Samuel Holloway <sholloway@gmail.com>
2020-05-03 09:38:42 -0500

    Added clean-webpack-plugin to the project for managing the dist directory.

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,14 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: './lib/index.js',
 	output: {
 		filename: 'conways-game.js',
-		path: path.resolve(__dirname, 'public','dist'),
-		library: 'Conways'
+		path: path.resolve(__dirname, 'public', 'dist'),
+		library: 'Conways',
 	},
+	plugins: [new CleanWebpackPlugin()],
 	// module: {
 	// 	rules: [
 	// 		{
