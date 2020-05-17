75a0acc64c3227225b257a9d61371a191ace7ef8
Samuel Holloway <sholloway@gmail.com>
2020-05-17 10:17:13 -0500

    Styled the start button.

--- src/lib/components/StartButton.js
+++ src/lib/components/StartButton.js
@@ -46,7 +46,18 @@ class StartButton extends LitElement {
 	 * @returns {string}
 	 */
 	static get styles() {
-		return css``;
+		return css`
+			button {
+				cursor: pointer;
+				height: 25px;
+				border-radius: 5px;
+				border: 1px;
+				border-style: solid;
+				border-color: rgb(216, 216, 216);
+				margin-left: 2px;
+				margin-right: 2px;
+			}
+		`;
 	}
 
 	/**
